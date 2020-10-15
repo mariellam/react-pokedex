7a2fd132d2ac8bf505c22a33c9d47e826638092a
Maku Miranda <mariella@able.co>
2020-10-15 14:03:53 -0500

    Hooks for search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
 import Pokedex from './Pokedex'
 
 function App() {
   return (
-    <div className="main">
-      <Header />
-      <Pokedex />
-    </div>
+    <Pokedex />
   );
 }
 
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,7 +4,11 @@ import Pokemon from './Pokemon'
 function Pokedex() {
   const [isLoading, setIsLoading] = useState(true)
   const [pokemons, setPokemons] = useState([]);
-  
+  const [search, setSearch] = useState('');
+
+  const handleChange = event => {
+    setSearch(event.target.value);
+  };
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
@@ -15,13 +19,26 @@ function Pokedex() {
     })
   }, [isLoading]);
 
-  const pokemonList = pokemons.map((pokemon,i) => (<Pokemon key={i} pokemonId={pokemon.url.slice(34, -1)} pokemonName={pokemon.name} pokemonUrl={pokemon.url} />))
+  const pokemonList = pokemons
+                        .filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+                        .map((pokemon,i) => (<Pokemon key={i} pokemonId={pokemon.url.slice(34, -1)} pokemonName={pokemon.name} pokemonUrl={pokemon.url} />))
 
   return (
-    <div className="container">
-      <ul className="pokedex">
-        {pokemonList}
-      </ul>
+    <div className="main">
+      <header className="container">
+        <div className="nav-container">
+          <h1 className="title">Maku's Pokedex</h1>
+          <div className="search">
+            <input className="searchbox" name="search" type="text" placeholder="Search Pokemon" onChange={handleChange} />
+          </div>
+        </div>
+      </header>
+      <div className="container">
+        {isLoading && <p>Loading Pokemon Information</p>}
+        <ul className="pokedex">
+          {pokemonList}
+        </ul>
+      </div>
     </div>
   )
 }
